3b96fcf966adab74431ccb4f7b77deddd90bee20
shellinw <143944980+shellinw@users.noreply.github.com>
2024-12-20 23:43:46 +0700

    revised config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 3000 || 5000;
 const todosController = require("./Controller/todosController");
 const errorhandler = require("./errorhandler");
 app.use(cors());
